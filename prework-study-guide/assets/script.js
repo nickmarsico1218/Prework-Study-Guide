6246f57e69caccd32ccecd76f5a6d1a634fded98
Nick <your_nwmarsico@gmail.com>
2024-03-11 18:42:24 -0500

    <made changes to javascript>

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,31 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (let x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+}
+
+/*
+
+if (topic === 'HTML') {
+
+    console.log("Let's study HTML!");
+
+} else is (topic === 'CSS') {
+
+    console.log("Let's study CSS!");
+
+} else is topic === 'Git') {
+
+    console.log("Let's study Git!");
+
+} else if (topic === 'JavaScript') {
+
+    console.log("Let's study JavaScript!");
+
+} else {
+
+    console.log('Plese try again!');
+}
+
+*/
